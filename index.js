0077c55a334debae3b3adb19dec0fa769cee4de6
Leigh-Ann Friedel <leigh.ann.friedel@gmail.com>
2019-02-11 17:59:35 -0500

    Successful POST to /api/users

--- index.js
+++ index.js
@@ -4,40 +4,43 @@ const server = express();
 
 server.use(express.json());
 
-
-
-// POST to /api/users -- I'm getting the right responses, but it's still inserting the user no matter what
+// POST to /api/users
 server.post("/api/users", (req, res) => {
-  const user = req.body;
+  const { name, bio } = req.body;
+  const newUser = { name, bio };
 
-if (user.name && user.bio) {
-
+  if (!name || !bio) {
+    return res
+      .status(400)
+      .json({ errorMessage: "Please probide name and bio for the user." });
   }
 
-  db.insert(user)
-    .then(newUser => {
-      if (user.name && user.bio) {
-        res.status(201).json({ success: true, newUser });
-      } else {
-        res
-          .status(400)
-          .json({
-            success: false,
-            error: "Please provide name and bio for the user."
-          });
-      }
+  db.insert(newUser)
+    .then(newUserId => {
+
+      const { id } = newUserId;
+      
+      db.findById(id)
+        .then(user => {
+          res
+            .status(201)
+            .json({ user });
+        })
+        .catch(err => {
+          res
+            .status(500)
+            .json({
+              success: false,
+              error: "The user information could not be retrieved."
+            });
+        });
     })
     .catch(err => {
-      res
-        .status(500)
-        .json({
-          success: false,
-          error: "There was an error while saving the user to the database."
-        });
+      res.status(500).json({
+        error: "There was an error while saving the user to the database."
+      });
     });
 });
-
-
 
 // GET to /api/users
 server.get("/api/users", (req, res) => {
@@ -46,19 +49,12 @@ server.get("/api/users", (req, res) => {
       res.status(200).json({ success: true, users });
     })
     .catch(err => {
-      res
-        .status(500)
-        .json({
-          success: false,
-          error: "The information could not be retrieved."
-        });
+      res.status(500).json({
+        success: false,
+        error: "The information could not be retrieved."
+      });
     });
 });
-
-server.listen(5000, () => {
-  console.log("Running on port 5000");
-});
-
 
 
 // GET to /api/users/:id
@@ -68,50 +64,62 @@ server.get("/api/users/:id", (req, res) => {
   db.findById(userId)
     .then(user => {
       if (user) {
-        res.status(200).json({ success: true, user });
+        return res.status(200).json({ success: true, user });
       } else {
-        res
-          .status(404)
-          .json({
-            success: false,
-            message: "The user with the specified ID does not exist."
-          });
+        res.status(404).json({
+          success: false,
+          message: "The user with the specified ID does not exist."
+        });
       }
     })
     .catch(err => {
-        res.status(500).json({ success: false, error: "The user information could not be retrieved." });
+      res
+        .status(500)
+        .json({
+          success: false,
+          error: "The user information could not be retrieved."
+        });
+    });
+});
+
+// DELETE to /api/users/:id
+server.delete("/api/users/:id", (req, res) => {
+  const userId = req.params.id;
+
+  db.findById(userId)
+    .then(user => {
+      if (user) {
+        db.remove(userId)
+          .then(deletedUser => {
+            res.status(200).json({ success: true, user });
+          })
+          .catch(err => {
+            res
+              .status(500)
+              .json({
+                success: false,
+                message: "The user could not be removed."
+              });
+          });
+      } else {
+        res.status(404).json({
+          success: false,
+          message: "The user with the specified ID does not exist."
+        });
+      }
+    })
+
+    .catch(err => {
+      res
+        .status(500)
+        .json({
+          success: false,
+          error: "The user information could not be retrieved."
+        });
     });
 });
 
 
-
-// DELETE to /api/users/:id
-server.delete("/api/users/:id", (req, res) => {
-    
-    const userId = req.params.id;
-  
-    db.findById(userId)
-        .then(user => {
-        if (user) {
-            db.remove(userId)
-            .then(deletedUser => {
-                    res.status(200).json({ success: true, user })
-            })
-            .catch(err => {
-                res.status(500).json({ success: false, message: "The user could not be removed." });
-            });
-        } else {
-            res
-            .status(404)
-            .json({
-                success: false,
-                message: "The user with the specified ID does not exist."
-            });
-        };
-
-        })
-
-        .catch(err => {
-            res.status(500).json({ success: false, error: "The user information could not be retrieved." });
-        });
-})
+server.listen(5000, () => {
+    console.log("Running on port 5000");
+  });
