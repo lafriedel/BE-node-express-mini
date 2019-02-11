1a6e62402ac8be6c114c4bf937bc338deaa47b73
Leigh-Ann Friedel <leigh.ann.friedel@gmail.com>
2019-02-11 15:27:13 -0500

    Successful GET to /api/users

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// implement your API here
+const express = require("express");
+const db = require("./data/db");
+const server = express();
+
+server.use(express.json());
+
+// POST to /api/users
+// server.post("/api/users", (req, res) => {
+
+// })
+
+// GET to /api/users
+server.get("/api/users", (req, res) => {
+    db
+        .find()
+        .then(users => {
+            res.status(200).json({ success: true, users });
+        })
+        .catch(err => {
+            res.status(500).json({ success: false, error: "The information could not be retrieved." })
+        });
+})
+
+server.listen(5000, () => {
+    console.log("Running on port 5000");
+})
